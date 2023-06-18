f8b3b7d4199a000c69a4b3cb39e9fbac3408f66d
juao29 <joao.vpcsetani@gmail.com>
2023-06-17 21:11:39 -0300

    今度、特に何もしてない

--- kanban/src/app/view/kanban/kanban.component.ts
+++ kanban/src/app/view/kanban/kanban.component.ts
@@ -18,17 +18,6 @@ export class KanbanComponent implements OnInit {
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX – The Rise of Skywalker',
-  ];
 
   listar() {
     this.tasksService.listar().subscribe(tasks => {
@@ -48,8 +37,6 @@ export class KanbanComponent implements OnInit {
     });
   }
 
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
-  }
+ 
   
 }
